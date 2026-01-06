ffbfbd5b2bd028d5121f61ef880e9552001064d8
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2026-01-06 08:11:34 +0330

    add is open button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 export default function App() {
   const [step, setStep] = useState(1)
+  const [isOpen, setIsOpen] = useState(true)
 
   const messages = [
     "Learn React ⚛️",
@@ -23,21 +24,25 @@ export default function App() {
     }
   }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
-      <p className="message">{messages[step - 1]}</p>
-      <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "white" }} onClick={handlePrev}>
-          Prev
-        </button>
-        <button style={{ backgroundColor: "#7950f2", color: "white" }} onClick={handleNext}>
-          Next
-        </button>
-      </div>
-    </div>
-  );
+    <>
+      <button className="close" onClick={()=>setIsOpen(!isOpen)}>&times;</button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">{messages[step - 1]}</p>
+          <div className="buttons">
+            <button style={{ backgroundColor: "#7950f2", color: "white" }} onClick={handlePrev}>
+              Prev
+            </button>
+            <button style={{ backgroundColor: "#7950f2", color: "white" }} onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>)};
+    </>
+  )
 }
